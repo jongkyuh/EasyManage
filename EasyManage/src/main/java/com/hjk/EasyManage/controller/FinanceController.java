144c254fcb80315c4d71653682b374b3883373e0
jongkyuh <jongkyuh@naver.com>
2025-06-19 23:16:09 +0900

    feat. webclient 빈 등록, 가계부 api 연동테스트
    가계부 기능을 위한 프로젝트와의 연동테스트

---
+++ EasyManage/src/main/java/com/hjk/EasyManage/controller/FinanceController.java
@@ -0,0 +1,37 @@
+package com.hjk.EasyManage.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/finance")
+public class FinanceController {
+
+    private final WebClient.Builder webClientBuilder;
+    @GetMapping("")
+    public String goFinance(HttpSession httpSession, Model model){
+
+        long loginId = (long) httpSession.getAttribute("loginId");  // 로그인한 아이디값
+
+        String username = webClientBuilder
+                .baseUrl("http://localhost:8082/api/finance")
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder.queryParam("userId", loginId).build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        model.addAttribute("username",username);
+        return "finance/finance";
+    }
+
+
+
+}
