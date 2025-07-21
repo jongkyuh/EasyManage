65311a06b060d7098b3331749c43aae5a6295214
jongkyuh <jongkyuh@naver.com>
2025-07-21 22:01:53 +0900

    feat. 가계부리스트 생성을 위한 컴포넌트 추가, API구현
    1. 가계부리스트 생성 컴포넌트 추가
    2. 가계부리스트로 이동할 컨트롤러 추가 -> api요청 구현

--- EasyManage/src/main/java/com/hjk/EasyManage/controller/finance/FinanceListController.java
+++ EasyManage/src/main/java/com/hjk/EasyManage/controller/finance/FinanceListController.java
@@ -1,5 +1,6 @@
 package com.hjk.EasyManage.controller.finance;
 
+import com.hjk.EasyManage.dto.finance.category.FinanceCategoryResponse;
 import com.hjk.EasyManage.dto.finance.list.FinanceListCreateDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,9 +27,24 @@ public class FinanceListController {
 
     private final WebClient.Builder webClientBuilder;
     @GetMapping("")
-    public String goHome(Model model){
+    public String goHome(Model model, HttpSession httpSession){
+
+        long loginId = (long) httpSession.getAttribute("loginId");
+        List<FinanceCategoryResponse> categoryList = webClientBuilder.baseUrl("http://localhost:8083")
+                .build()
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/finance/category/list")
+                        .queryParam("userId", loginId)
+                        .build())
+                .retrieve()
+                .bodyToFlux(FinanceCategoryResponse.class)
+                .collectList()
+                .block();
+
 
         model.addAttribute("listForm", new FinanceListCreateDto());
+        model.addAttribute("categoryList",categoryList);
         return "finance/financeList";
     }
 
@@ -36,15 +54,17 @@ public class FinanceListController {
         long userId = (long) httpSession.getAttribute("loginId");
         financeListCreateDto.setUserId(userId);
 
+
         webClientBuilder.baseUrl("http://localhost:8083")
                 .build()
                 .post()
                 .uri("/api/finance/list/create")
+                .bodyValue(financeListCreateDto)
                 .retrieve()
                 .bodyToMono(ResponseEntity.class)
                 .block();
 
-        return "redirect::/finance/list";
+        return "redirect:/finance/list";
 
     }
 
